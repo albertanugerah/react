b500df3d0fd77a29773794fa767c80284b0b579e
albertanugerah <albertanugerah@hotmail.com>
2021-02-08 22:49:34 +0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({hi :'there'})
+  res.send({bye :'buddy'})
 })
 
 // process.env.PORT heroku inject process.env
