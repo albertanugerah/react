0c4777487884c9b50886df48e8fb516ea5ea5f0e
albertanugerah <albertanugerah@hotmail.com>
2021-02-11 00:29:25 +0700

    change passportjs with create profile to mongodb

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,30 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
-      clientServer: keys.googleClientSecret,
+      clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token :", accessToken);
-      console.log("refreshToken :", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+        } else {
+          // we dont have a user record with this ID make a new Record
+          new User({ googleId: profile.id }).save();
+        }
+      });
+
+      // console.log("access token", accessToken);
+      // console.log("refresh token", refreshToken);
+      // console.log("profile", profile);
     }
   )
 );
