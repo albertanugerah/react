1be23146235b5e2695250402b2a945554889833d
albertanugerah <albertanugerah@hotmail.com>
2021-02-11 19:43:11 +0700

    refactoring development and production file

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!!!!
+module.exports = {
+  googleClientID:
+    "826408197768-0bq5spqe7hvkulfq7qioqgsugjur2kko.apps.googleusercontent.com",
+  googleClientSecret: "G2RNLy1tlPhF3v-r4DVVeQCp",
+  mongoURI:
+    "mongodb+srv://root:root@emaily.uzy24.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "albertanugerahganteng",
+};
