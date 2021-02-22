4d766c802b39a9cdb0fcb1b2bf069329cff1a236
albertanugerah <albertanugerah@hotmail.com>
2021-02-22 23:18:31 +0700

    authReducer Return Values

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {} from 'module';
 
 class Header extends Component {
   render() {
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default function (state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+export default function (state = null, action) {
+  console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
