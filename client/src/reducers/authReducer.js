d96630b3fbef8d3ccee92cd7e73917195b69dc1d
albertanugerah <albertanugerah@hotmail.com>
2021-02-24 00:44:10 +0700

    adding Stripe API Key into heroku and config/dev.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { FETCH_USER } from '../actions/types';
+
 export default function (state = null, action) {
   console.log(action);
   switch (action.type) {
